074a30da33057e5e38582a708795d1bf052b9c69
221 <221@221-007>
2025-10-21 13:21:55 +0300

    Добавьте файлы проекта.

---
+++ LabWork10/Password.cs
@@ -0,0 +1,8 @@
+﻿namespace LabWork10
+{
+    public class Password
+    {
+        public static bool IsValid(string password) 
+            => password.Length >= 8 && password.Any(char.IsDigit) && password.Any(char.IsLetter);
+    }
+}
---
+++ ValidatingTest/PasswordTest.cs
@@ -0,0 +1,29 @@
+using LabWork10;
+
+namespace ValidatingTest
+{
+    public class PasswordTest
+    {
+        [Theory]
+        [InlineData("password1")]
+        [InlineData("passw0rd")]
+        [InlineData("1234567a")]
+        [InlineData("parolcarol1234679890228!")]
+        [InlineData("adminka!1703")]
+        public void CorrectPassword_ReturnsTrue(string password)
+        {
+            Assert.True(Password.IsValid(password));
+        }
+
+        [Theory]
+        [InlineData("pswrd")]
+        [InlineData("")]
+        [InlineData("@34as")]
+        [InlineData("@@@@@@@@@@@@@@@@@3344334")]
+        [InlineData("!!!!!asd")]
+        public void IncorrectPassword_ReturnsFalse(string password)
+        {
+            Assert.False(Password.IsValid(password));
+        }
+    }
+}
